c0aef52274b1a0f9d38aeb13071935b2e64f4546
Priyanka-390 <psr82220@gmail.com>
2024-07-12 12:44:01 +0530

    folder structure

---
+++ src/utils/Helper.tsx
@@ -0,0 +1,19 @@
+
+export const MEET_DATA_LIST = [
+    {
+        title: "Hello",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, quo."
+    },
+    {
+        title: "Hello1",
+        description:"Lorem ipsum ipsum dolor sit amet consectetur adipisicing elit. Temporibus, quo."
+    },
+    {
+        title: "Hello2",
+        description:"Lorem  ipsum dolor ipsum dolor sit amet consectetur adipisicing elit. Temporibus, quo."
+    },
+    {
+        title: "Hello3",
+        description:"Lorem ipsum dolor ipsum dolor sit amet consectetur adipisicing elit. Temporibus, quo."
+    },
+]
